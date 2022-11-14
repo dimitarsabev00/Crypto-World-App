ca822721dd030396e950a0c7b872fdfb93dacb86
Dimitar <wopz1337@gmail.com>
2022-11-14 11:49:48 +0200

    Added Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,56 @@
-const Header = () => {
-  return <div>Header</div>;
-};
+import {
+  AppBar,
+  Container,
+  createTheme,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router";
+
+function Header() {
+  const navigate = useNavigate();
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate(`/`)}
+              variant="h6"
+              sx={{
+                flex: 1,
+                color: "gold",
+                fontFamily: "Montserrat",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Crypto Hunter
+            </Typography>
+            <Select
+              variant="outlined"
+              sx={{
+                width: 100,
+                height: 40,
+                marginLeft: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"EURO"}>EURO</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
 
 export default Header;
