08b7b0d9c5bebaa8e595b71b0842dcf5e462f863
Dimitar <wopz1337@gmail.com>
2022-11-14 11:08:48 +0200

    Added Router In App.jsx & Added Diferent Pages And Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CoinPage from "./pages/CoinPage";
+import Header from "./components/Header";
+import ErrorPage from "./pages/ErrorPage";
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/coins/:id" element={<CoinPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
