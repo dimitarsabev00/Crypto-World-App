aee8f76171af5c1d1f770843b117d8b42746c148
Dimitar <wopz1337@gmail.com>
2022-11-15 11:42:36 +0200

    Added Coin Page Logic

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -1,5 +1,161 @@
+import { useParams } from "react-router";
+import { AppState } from "../context/AppContext";
+import { SingleCoin } from "../configs/api";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { LinearProgress, Typography } from "@mui/material";
+// import ReactHtmlParser from "react-html-parser";
+import { numberWithCommas } from "../components/CoinsTable";
+import CoinInfo from "../components/CoinInfo";
+
 const CoinPage = () => {
-  return <div>CoinPage</div>;
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = AppState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+  if (!coin) return <LinearProgress style={{ backgroundColor: "gold" }} />;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      <div
+        style={{
+          width: "30%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 25,
+          borderRight: "2px solid grey",
+        }}
+      >
+        <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          height="200"
+          style={{ marginBottom: 20 }}
+        />
+        <Typography
+          variant="h3"
+          style={{
+            fontWeight: "bold",
+            marginBottom: 20,
+            fontFamily: "Montserrat",
+          }}
+        >
+          {coin?.name}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{
+            width: "100%",
+            fontFamily: "Montserrat",
+            padding: 25,
+            paddingBottom: 15,
+            paddingTop: 0,
+            textAlign: "justify",
+          }}
+        >
+          {coin?.description.en.split(". ")[0]}
+        </Typography>
+        <div
+          style={{
+            alignSelf: "start",
+            padding: 25,
+            paddingTop: 10,
+            width: "100%",
+          }}
+        >
+          <span style={{ display: "flex" }}>
+            <Typography
+              variant="h5"
+              style={{
+                fontWeight: "bold",
+                marginBottom: 20,
+                fontFamily: "Montserrat",
+              }}
+            >
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {numberWithCommas(coin?.market_cap_rank)}
+            </Typography>
+          </span>
+
+          <span style={{ display: "flex" }}>
+            <Typography
+              variant="h5"
+              style={{
+                fontWeight: "bold",
+                marginBottom: 20,
+                fontFamily: "Montserrat",
+              }}
+            >
+              Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.current_price[currency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
+          <span style={{ display: "flex" }}>
+            <Typography
+              variant="h5"
+              style={{
+                fontWeight: "bold",
+                marginBottom: 20,
+                fontFamily: "Montserrat",
+              }}
+            >
+              Market Cap:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.market_cap[currency.toLowerCase()]
+                  .toString()
+                  .slice(0, -6)
+              )}
+              M
+            </Typography>
+          </span>
+        </div>
+      </div>
+      <CoinInfo coin={coin} />
+    </div>
+  );
 };
 
 export default CoinPage;
